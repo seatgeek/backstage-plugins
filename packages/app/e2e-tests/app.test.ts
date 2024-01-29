e009a2531561ed79e1e7bcd30abf2ec100dba97b
Zach Hammer <zhammer@seatgeek.com>
2024-01-29 13:16:35 -0500

    format

--- packages/app/e2e-tests/app.test.ts
+++ packages/app/e2e-tests/app.test.ts
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 test('App should render the welcome page', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page.getByText('SeatGeek Backstage Plugins Demo Site')).toBeVisible();
+  await expect(
+    page.getByText('SeatGeek Backstage Plugins Demo Site'),
+  ).toBeVisible();
 });
