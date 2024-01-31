1217ee4950ac113054de5549a6ecbcd4e5b43352
Zach Hammer <zhammer@seatgeek.com>
2024-01-30 22:42:26 -0500

    fix peer dependency issues (#22)
    * remove version-bridge
    
    * update react version
    
    * fix peer dependencies
    
    * add job checking for peer dependency mismatches
    
    * fix job

--- plugins/awards/src/components/Cards/User/UserAwardsCard/UserAwardsCard.tsx
+++ plugins/awards/src/components/Cards/User/UserAwardsCard/UserAwardsCard.tsx
@@ -8,35 +8,20 @@ import {
   Progress,
   ResponseErrorPanel,
 } from '@backstage/core-components';
-import {
-  createRouteRef,
-  useApi,
-  useRouteRefParams,
-} from '@backstage/core-plugin-api';
+import { useApi } from '@backstage/core-plugin-api';
 import { Box, Grid, Tooltip } from '@material-ui/core';
 import React from 'react';
 import useAsync from 'react-use/lib/useAsync';
 import { awardsApiRef } from '../../../../api';
 
-import { getOrCreateGlobalSingleton } from '@backstage/version-bridge';
+import { stringifyEntityRef } from '@backstage/catalog-model';
+import { useEntity } from '@backstage/plugin-catalog-react';
 import { Award } from '@seatgeek/plugin-awards-common';
-
-// https://github.com/backstage/backstage/blob/77a7ef3fa500fcfb80bee21f971eee682a955655/plugins/catalog-react/src/routes.ts#L28
-// https://github.com/backstage/backstage/blob/77a7ef3fa500fcfb80bee21f971eee682a955655/plugins/catalog/src/components/CatalogEntityPage/useEntityFromUrl.ts#L31
-// https://github.com/backstage/backstage/blob/77a7ef3fa500fcfb80bee21f971eee682a955655/plugins/catalog/src/components/CatalogEntityPage/CatalogEntityPage.tsx#L23
-export const entityRouteRef = getOrCreateGlobalSingleton(
-  'awards:entity-route-ref',
-  () =>
-    createRouteRef({
-      id: 'catalog:entity',
-      params: ['namespace', 'kind', 'name'],
-    }),
-);
 
 // For a reference, check https://github.com/backstage/backstage/blob/77a7ef3fa500fcfb80bee21f971eee682a955655/plugins/org/src/components/Cards/User/UserProfileCard/UserProfileCard.tsx
 /** @public */
 export const UserAwardsCard = () => {
-  const { kind, namespace, name } = useRouteRefParams(entityRouteRef);
+  const { entity } = useEntity();
   const awardsApiClient = useApi(awardsApiRef);
 
   const {
@@ -44,12 +29,7 @@ export const UserAwardsCard = () => {
     loading,
     error,
   } = useAsync(async (): Promise<Award[]> => {
-    return awardsApiClient.getAwards(
-      '',
-      '',
-      [],
-      [`${kind}:${namespace}/${name}`],
-    );
+    return awardsApiClient.getAwards('', '', [], [stringifyEntityRef(entity)]);
   }, []);
 
   if (loading) {
@@ -76,9 +56,6 @@ export const UserAwardsCard = () => {
                       />
                     </Tooltip>
                   </Box>
-                  {/* <Box sx={{ width: '100%', textAlign: 'center' }}>
-                        {award.name}
-                      </Box> */}
                 </Box>
               </Link>
             </Grid>
