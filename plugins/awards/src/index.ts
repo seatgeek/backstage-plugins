2b3b84162c10321ae7d44d30153ce44991c5c1e7
Zach Hammer <zhammer@seatgeek.com>
2024-02-12 09:42:16 -0500

    run add copyrights script

--- plugins/awards/src/index.ts
+++ plugins/awards/src/index.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright SeatGeek
+ * Licensed under the terms of the Apache-2.0 license. See LICENSE file in project root for terms.
+ */
 export * from './api';
 export { UserAwardsCard } from './components';
 export { AwardsPage, awardsPlugin } from './plugin';
