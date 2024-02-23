d44f82dbf59b85ec7ebf31bbd2804dfbaf7beb7c
Zach Hammer <zhammer@seatgeek.com>
2024-02-23 10:48:23 -0500

    addf slack notification gateway

--- plugins/awards-backend/src/notifications/notifications.ts
+++ plugins/awards-backend/src/notifications/notifications.ts
@@ -1,8 +1,26 @@
 import { Award } from "@seatgeek/backstage-plugin-awards-common";
+import { IncomingWebhook } from '@slack/webhook';
 
 /**
  * Interface for sending notifications about awards
  */
 export interface NotificationsGateway {
   notifyNewRecipientsAdded(identityRef: string, award: Award, newRecipients: string[]): Promise<void>;
+}
+
+export class SlackNotificationsGateway implements NotificationsGateway {
+  private readonly slack: IncomingWebhook;
+
+  constructor(slack: IncomingWebhook) {
+    this.slack = slack;
+  }
+
+  async notifyNewRecipientsAdded(_: string, award: Award, newRecipients: string[]): Promise<void> {
+    await this.slack.send({
+      text: `🎉🎉🎉 Woohoo! The following users have received the ${award.name} award! 🎉🎉🎉
+
+${newRecipients.map(recipient => `- ${recipient}`).join('\n')}
+      `
+    })
+  }
 }
