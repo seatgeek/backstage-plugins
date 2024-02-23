466a7d9cd62b4f4623b54bca2abf04bf91b606d5
Zach Hammer <zhammer@seatgeek.com>
2024-02-23 03:31:27 -0500

    fix tsc

--- plugins/awards-backend/src/service/router.ts
+++ plugins/awards-backend/src/service/router.ts
@@ -3,9 +3,8 @@
  * Licensed under the terms of the Apache-2.0 license. See LICENSE file in project root for terms.
  */
 import { PluginDatabaseManager, errorHandler } from '@backstage/backend-common';
-import { AuthenticationError, NotFoundError } from '@backstage/errors';
+import { AuthenticationError } from '@backstage/errors';
 import { IdentityApi } from '@backstage/plugin-auth-node';
-import { Award } from '@seatgeek/backstage-plugin-awards-common';
 import express from 'express';
 import Router from 'express-promise-router';
 import { Logger } from 'winston';
