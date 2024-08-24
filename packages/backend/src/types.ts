d01d3b94202bb11154b4ac5d97ca371fb7d4b2c5
Zach Hammer <zhammer@seatgeek.com>
2024-08-24 16:25:30 -0400

    bump backstage 1.22 -> 1.30

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -3,7 +3,6 @@ import {
   PluginDatabaseManager,
   PluginEndpointDiscovery,
   TokenManager,
-  UrlReader,
 } from '@backstage/backend-common';
 import { PluginTaskScheduler } from '@backstage/backend-tasks';
 import { Config } from '@backstage/config';
@@ -16,7 +15,6 @@ export type PluginEnvironment = {
   database: PluginDatabaseManager;
   cache: PluginCacheManager;
   config: Config;
-  reader: UrlReader;
   discovery: PluginEndpointDiscovery;
   tokenManager: TokenManager;
   scheduler: PluginTaskScheduler;
