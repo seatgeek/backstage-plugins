562767cceb6e6469eea5d688e81d960b1589ba9e
Brianna Becker <bbecker@seatgeek.com>
2024-09-13 15:03:05 +0200

    Add eslint file

---
+++ packages/node-hcl/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+  },
+  env: {
+    browser: false,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    "wasm/**/*"
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+};
