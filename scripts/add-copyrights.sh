ba1fc7ab09c2448c391aa2f74bc9c86c972724f6
Brianna Becker <bbecker@seatgeek.com>
2024-09-13 12:47:16 +0200

    Update copyright script for new package

--- scripts/add-copyrights.sh
+++ scripts/add-copyrights.sh
@@ -9,10 +9,11 @@ read -r -d '' COPYRIGHT_HEADER << EOM
 EOM
 
 # Loop over .ts and .tsx files in the plugins/ directory
-for file in $(find plugins -type f \( -name "*.ts" -o -name "*.tsx" \)); do
+for file in $(find . \( -path "./plugins/*" -o \( -path "./packages/*" -not -path "./packages/backend/*" -not -path "./packages/app/*" \) \) -type f \( -name "*.ts" -o -name "*.tsx" \)); do
     # Check if the file is ignored by Git
     if ! git check-ignore $file > /dev/null 2>&1; then
         # If it's not ignored, check if the copyright header already exists in the file
+        echo $file
         if ! [[ "$(head -n4 $file)" == *"$COPYRIGHT_HEADER"* ]]; then
             echo $file Δ
             echo "${COPYRIGHT_HEADER}
